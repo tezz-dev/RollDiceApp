e1fd83fa614ceaeb24389b53ca4d28ecbf658daa
Tejas M R <matblockgreninja@gmail.com>
2020-12-12 16:30:31 +0530

    Initial Commit

---
+++ RollDiceApp/RollDiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceAppApp.swift
+//  RollDiceApp
+//
+//  Created by tezz on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
